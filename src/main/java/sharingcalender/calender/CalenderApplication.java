2ca385f4dfd4fda425a3191eabd45773b7bd6285
kimminjun <jj36282013@gmail.com>
2025-01-23 02:20:03 +0900

    init

---
+++ src/main/java/sharingcalender/calender/CalenderApplication.java
@@ -0,0 +1,15 @@
+package sharingcalender.calender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CalenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalenderApplication.class, args);
+    }
+
+}

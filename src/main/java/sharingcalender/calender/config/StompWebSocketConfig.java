7d98b33d78391efffb8ea3a3484cd804b3aca953
kimminjun <jj36282013@gmail.com>
2025-07-27 18:18:58 +0900

    Feature: add prometheus dependency

--- src/main/java/sharingcalender/calender/config/StompWebSocketConfig.java
+++ src/main/java/sharingcalender/calender/config/StompWebSocketConfig.java
@@ -28,6 +28,9 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        // 정의된 prefix 를 가지는 destination 의 경우, @MessageMapping 애노테이션 기반 메서드에서 처리
+        // 단, @MessageMapping 에 경로를 작성할 때 prefix 를 제외하고 작성해야한다.
         registry.setApplicationDestinationPrefixes("/publish");
 
         registry.enableSimpleBroker("/topic","/queue");
